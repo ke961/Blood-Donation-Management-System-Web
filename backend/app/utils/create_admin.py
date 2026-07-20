46696613fedc5b84e036d67996f9f20c4dc111b6
Abila Khan <khankeya961@gmail.com>
2026-07-20 22:11:43 +0600

    Update create_admin.py
    updated

--- backend/app/utils/create_admin.py
+++ backend/app/utils/create_admin.py
@@ -7,12 +7,15 @@ def create_admin(db: Session):
     admin = db.query(User).filter(User.email == "admin@gmail.com").first()
 
     if not admin:
-
         admin = User(
             full_name="Admin",
             email="admin@gmail.com",
             password=hash_password("admin123"),
-            role="admin"
+            role="admin",
+            phone="01700000000",
+            blood_group="O+",
+            gender="Male",
+            address="Dhaka"
         )
 
         db.add(admin)
