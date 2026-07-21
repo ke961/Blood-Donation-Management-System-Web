da5b43d9676be64916cb70dcfea0683a634d5574
Abila Khan <khankeya961@gmail.com>
2026-07-21 21:29:43 +0600

    Update main.py
    Allow Vercel frontend in backend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -61,19 +61,29 @@ app = FastAPI(
 
 
 # Allow React frontend
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:5173",
+#         "http://localhost:5174",
+#         "http://127.0.0.1:5173",
+#         "http://127.0.0.1:5174",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
+        "http://localhost:5175",
+        "https://blood-donation-management-system-we.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
