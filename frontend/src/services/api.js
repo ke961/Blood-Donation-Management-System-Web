41e754d0450ab6b88f313ee3cfc293d9e07e7db4
Abila Khan <khankeya961@gmail.com>
2026-07-22 16:59:55 +0600

    Update api.js
    fix api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -100,28 +100,53 @@
 
 
 
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL:
+//     import.meta.env.VITE_API_URL ||
+//     "https://blood-donation-management-system-web.onrender.com",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("access_token");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default api;
+
+
 import axios from "axios";
 
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
     "https://blood-donation-management-system-web.onrender.com",
+
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access_token");
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
 export default api;
