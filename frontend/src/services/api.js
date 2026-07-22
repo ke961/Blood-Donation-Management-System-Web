2ab27f07d48017773a6e62e0a91c574d0b23bfff
Abila Khan <khankeya961@gmail.com>
2026-07-22 16:51:15 +0600

    Update api.js
    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -105,7 +105,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://127.0.0.1:8000",
+    "https://blood-donation-management-system-web.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
